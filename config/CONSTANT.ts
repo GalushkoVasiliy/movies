38446ea56b15d4fad59f2b303bf8bb824ec0f425
VasylHalushko <vasiliy.h@mintymint.net>
2025-03-24 18:54:03 +0200

    delete old const

--- config/CONSTANT.ts
+++ config/CONSTANT.ts
@@ -2,7 +2,6 @@ import Constants from 'expo-constants';
 const { IMAGE_URL } = Constants.expoConfig?.extra || {};
 export const IMAGE_URI_SMALL_SIZE = `${IMAGE_URL}w200`;
 export const IMAGE_URI_BIG_SIZE = `${IMAGE_URL}w500`;
-export const API_URI = '';
 
 export const HORIZONTAL_STACK_CONFIG_BIG = {
   height: 240,
